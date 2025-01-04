68fe40a215bcf6d0f797d7f7c8dfe925417bf338
MD Shariful Islam <masom3456@gmail.com>
2025-01-04 14:04:58 +0600

    fix error

--- frontend/components/Dashboard/favariteHomes/MainContainer.tsx
+++ frontend/components/Dashboard/favariteHomes/MainContainer.tsx
@@ -5,9 +5,9 @@ import React from 'react'
 
 export default function MainContainer() {
     const homes = useAppSelector(state=>state.userReducer.favarite)
-    const showHouse = homes.map(item=>(
+    const showHouse = homes?.length?homes.map(item=>(
         <HomeCard home={item} key={item.house_id}/>
-    ))
+    )):[]
   return (
     <div className='mt-3 flex flex-col gap-3'>
       {showHouse}
