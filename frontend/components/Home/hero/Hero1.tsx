e7e8effe0cf574895fcf41a53911af8143f80154
MD Shariful Islam <masom3456@gmail.com>
2025-01-05 13:57:38 +0600

    fix some res issue

--- frontend/components/Home/hero/Hero1.tsx
+++ frontend/components/Home/hero/Hero1.tsx
@@ -12,10 +12,10 @@ export default function Hero1() {
       </div>
         <div className='w-full h-screen absolute top-0 left-0 flex flex-col justify-center place-items-center'>
           <motion.div initial={{y:20,opacity:0}} exit={{y:20,opacity:0}} transition={{duration:0.5,delay:1}} whileInView={{y:0,opacity:1}} className='flex place-items-center md:text-xl text-sm gap-4 text-white'><span className='text-primary_color'><FaHome/></span><span>Welcome to Ghor Khujo</span></motion.div>
-          <h1 data-aos="fade-up" className='md:text-6xl text-5xl font-bold text-white text-center my-5 leading-[1.2]'>Find Your Dream <br /> House By Us</h1>
+          <h1 data-aos="fade-up" className='md:text-6xl text-3xl font-bold text-white text-center my-5 leading-[1.2]'>Find Your Dream <br /> House By Us</h1>
 
           <div data-aos="fade-up" data-aos-delay={5}>
-            <Link href={""}   className='px-10 flex place-items-center gap-4 py-4 bg-primary_color text-white hover:bg-white hover:text-black transition-all duration-500 relative before:absolute before:bg-white before:w-full before:h-full before:left-0 before:top-0 before:-z-[1]'>
+            <Link href={""}   className='px-10 flex md:text-base text-sm place-items-center gap-4 py-4 bg-primary_color text-white hover:bg-white hover:text-black transition-all duration-500 relative before:absolute before:bg-white before:w-full before:h-full before:left-0 before:top-0 before:-z-[1]'>
             <span>Explore Now</span>
             <span><LuMoveRight/></span>
             </Link>
--- frontend/components/Home/hero/Hero2.tsx
+++ frontend/components/Home/hero/Hero2.tsx
@@ -10,10 +10,10 @@ export default function Hero2() {
       </div>
         <div className='w-full h-screen absolute top-0 left-0 flex flex-col justify-center place-items-start md:pl-16 pl-5'>
           <h2 className='flex place-items-center md:text-xl text-base gap-4 text-white'><span className='text-primary_color'><FaHome/></span><span>Welcome to Ghor Khujo</span></h2>
-          <h1 className='md:text-6xl text-5xl font-bold text-white text-start my-5 leading-[1.2]'>Find Your Dream <br /> House By Us</h1>
+          <h1 className='md:text-6xl text-3xl font-bold text-white text-start my-5 leading-[1.2]'>Find Your Dream <br /> House By Us</h1>
           <p className='text-base font-light mb-4 pl-7 border-l border-l-primary_color text-white lg:w-[40%] md:w-[60%] w-[80%]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic expedita asperiores accusantium distinctio numquam, illo iste tempora ipsum. Eius, dignissimos.</p>
           <div>
-            <Link href={""} className='px-10 flex place-items-center gap-4 py-4 bg-primary_color text-white hover:bg-white hover:text-black transition-all duration-500 relative before:absolute before:bg-white before:w-full before:h-full before:left-0 before:top-0 before:-z-[1]'>
+            <Link href={""} className='px-10 flex place-items-center gap-4 py-4 md:text-base text-sm bg-primary_color text-white hover:bg-white hover:text-black transition-all duration-500 relative before:absolute before:bg-white before:w-full before:h-full before:left-0 before:top-0 before:-z-[1]'>
             <span>Explore Now</span>
             <span><LuMoveRight/></span>
             </Link>
